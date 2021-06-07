6bf4e01830a096bde0623828c15c66ceedb5f5e0
Jorge Borges <JDBorges187@gmail.com>
2021-06-07 17:51:07 -0400

    redux store pulled into component

--- react-app/src/components/Albums/index.js
+++ react-app/src/components/Albums/index.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { getAlbums } from "../../store/albums";
+
 
 function AlbumsList() {
+    const albums = useSelector(state => state.albums)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        console.log(dispatch(getAlbums()))
+    }, [dispatch])
+
+    if (!Object.keys(albums).length) return (
+        <h1>Loading...</h1>
+    )
+
     return (
         <div>
             <h1>Albums</h1>
