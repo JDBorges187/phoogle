328a3bc7e2f43e2f76533589dc6bf8b3bbaa702b
Jorge Borges <JDBorges187@gmail.com>
2021-06-08 17:03:07 -0400

    photo select button and selected useState

--- react-app/src/components/Photos/index.js
+++ react-app/src/components/Photos/index.js
@@ -8,11 +8,22 @@ import "./PhotoGrid.css"
 
 function PhotoGrid() {
     const photos = useSelector(state => state.photos)
+    const [selected, setSelected] = useState([])
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(getPhotos())
     }, [dispatch])
+
+    const onDelete = (id) => {
+
+        console.log(`Delete(${id})`)
+    }
+
+    const onselect = (id) => {
+        console.log(`Select(${id})`)
+        setSelected([...selected, id])
+    }
 
     if (!photos) return (
         <h1>Loading...</h1>
@@ -22,10 +33,17 @@ function PhotoGrid() {
         <div className="main-photos">
             <PhotoForm />
             <h1>Photos</h1>
+            <p>{`${selected.length} Photos Selected`}</p>
             <div className="photo-grid">
                 {Object.values(photos).map((photo, i) => {
                     return (
-                        <img key={i} src={photo.photoUrl} />
+                        <div key={i} className="photo-card">
+                            <img src={photo.photoUrl} />
+                            <div className="photo-btns">
+                                <button onClick={() => onselect(photo.id)}>Select</button>
+                                <button onClick={() => onDelete(photo.id)}>Delete</button>
+                            </div>
+                        </div>
                     )
                 })}
             </div>
