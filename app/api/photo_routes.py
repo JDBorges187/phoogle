12a9b8a790db6bad47c5a9a9dcbcff323b944eac
Jorge Borges <JDBorges187@gmail.com>
2021-06-10 00:23:21 -0400

    backend route for uploading photo

--- app/api/photo_routes.py
+++ app/api/photo_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request
 from flask_login import current_user, login_required
 from app.models import User, Photo, db
 from app.forms import PhotoForm
+from app.aws import upload_file_to_s3, allowed_file, get_unique_filename
 
 photo_routes = Blueprint('photos', __name__)
 
@@ -32,3 +33,32 @@ def add_photos():
         db.session.commit()
         return {"photo": [photo.to_dict()]}
     return {'errors': ['Unauthorized']}, 401
+
+
+@photo_routes.route('/file', methods=['POST'])
+@login_required
+def upload_photo():
+    if "photo" not in request.files:
+        return {"errors": "image required"}, 400
+
+    photo = request.files["photo"]
+
+    if not allowed_file(photo.filename):
+        return {"errors": "file type not permitted"}, 400
+    
+    photo.filename = get_unique_filename(photo.filename)
+
+    upload = upload_file_to_s3(photo)
+
+    if "url" not in upload:
+        # if the dictionary doesn't have a url key
+        # it means that there was an error when we tried to upload
+        # so we send back that error message
+        return upload, 400
+
+    url = upload["url"]
+    # flask_login allows us to get the current user from the request
+    new_photo = Photo(owner=current_user, photoUrl=url)
+    db.session.add(new_photo)
+    db.session.commit()
+    return {"photo": new_photo.to_dict()}
