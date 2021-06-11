8408de5630074891e480f97a2e3f9197987a6fb0
Jorge Borges <JDBorges187@gmail.com>
2021-06-11 13:29:50 -0400

    fixed bug route had undefined, added albumId to dispatch

--- react-app/src/components/Photos/index.js
+++ react-app/src/components/Photos/index.js
@@ -25,7 +25,7 @@ function PhotoGrid() {
         if (location.pathname==='/'){
             dispatch(getPhotos())
         } else{
-            dispatch(getAlbumPhotos())
+            dispatch(getAlbumPhotos(albumId))
         }
     }, [dispatch])
 
